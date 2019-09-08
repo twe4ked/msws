4ba583bfd68a09802fe1dbbdea674b7f9b17d85f
Odin Dutton <odindutton@gmail.com>
2019-09-08 14:03:09 +1000

    Add seed() to example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,7 +1,7 @@
-use msws::Rand;
+use msws::{seed, Rand};
 
 fn main() {
-    let seed = 0xb5ad4eceda1ce2a9;
+    let seed = seed(0);
     let mut r = Rand::new(seed).expect("invalid seed");
     for i in 0..10 {
         println!("{}: {:#010x}", i, r.rand());
