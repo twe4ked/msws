bc3704cb4f99a0c40b5a638805c1f9db72530d4b
Odin Dutton <odindutton@gmail.com>
2019-09-08 13:55:22 +1000

    Print example numbers as hex

--- examples/basic.rs
+++ examples/basic.rs
@@ -4,6 +4,6 @@ fn main() {
     let seed = 0xb5ad4eceda1ce2a9;
     let mut r = Rand::new(seed).expect("invalid seed");
     for i in 0..10 {
-        println!("{}: {}", i, r.rand());
+        println!("{}: {:#010x}", i, r.rand());
     }
 }
